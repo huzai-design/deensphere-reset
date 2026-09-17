098a515e4d3573f21b13a30ea022a2f713a43a11
huzai-design <140350212+huzai-design@users.noreply.github.com>
2026-09-17 22:49:39 +0530

    Update Firebase private key and authorization token

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: 'deensphere-175d5',
     clientEmail: 'firebase-adminsdk-fbsvc@deensphere-175d5.iam.gserviceaccount.com',
-    privateKey: 're_ighSupGH_AD7nnC83TDS4XnSaDFHfhtz3'
+    privateKey: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDsGw9ZrPSYMDGi\n8zBAdR5mWxwWp6Jw1aOclQl5sZJLsmxz862926FrUGouoMHzfkjEQY9pg+sDsn2A\npm5qxtkDi5qTruDQvVHQGnSH1H8lcnxn47p2rdPcQ28xaKvPi5onb/5CFsCqo2Ab\nh/Sm+AdfKjxAHjRj4/E3O8z2MKVXfbkycz/rrv3IcDqUdrSLrYs65ECZOd4KvN4P\np56CtCaB3dbv4aKBazmh/nrVdI4SrNwJYbyRiGnxN44AUOx/Pp20um2L9liSCMuo\nBKq9KNi/IVMeoVs8dmJ1CpLXv3xy+bIi8XkNqxv7gGbCfJLrn0qGzkM4BV3R2csI\nGLUvc65rAgMBAAECggEAC/LQOdtdD99ogHTKkIiDMe55PNN3W6FEKjMH2FPAy2BL\nlfagAhRKVl73o98/N+GNMMYy/Sw2Fk+ctrtoqeV6qzypmI21RnsLCxTVQhNIt/Sg\nqeh5a3LsKzpyak9/8/0YBdgbriAh2Fpz2hDTgPVBkj+JIW/yrZdy6DbeuxrGubQQ\n0V8EXJzwin3GGzhQPqpcjzmr/pIgVsXtpMqKSgnjCX4joa9RDQJmo6wtwrr1nQy4\nKzjlHW7X2dh4LLTTuhRhpkkKrG+cp2A92qibKrKI/m2Sur8ZmB/kfjeIhCLuvw92\nkqwZEVZrt9Oqql1SsLSjYd4HcULQTR5QquwnIAe2WQKBgQD38wTpGsgek+c52bfs\nOrVK69OYeLJ2+4OnQ3xRZYtOp0ef159Y2/bVEEwWN2ClXzTZWTO4RFO8JBXs9lmk\ngcMWC1rhdCU88trNy6gvV1aQWQGprU4AKjFbp8eyAn3NOrsMfksFMHBGpnT3LseU\nfLjlpDIcPKtRDBDLp9QYtOz6lwKBgQDzxZh5JY2hm3K2FvOuiYwzxk1HPPClnrW3\nVhq7B8+zg/v4N0rUX5SfYVxTkOpuylozAxbxTbfsCdZ2WyrDQBZD8pYTXaZDDNMn\ntD//dSCm5Qmyln8v982V1lIulhkXx0W6869Jl/R3BVeRAx7MWgyoJ1Y6NcNOP44K\n93oJaCsJTQKBgBfu90xg72w0Lp+g6mX9DL7coKIZFHDzujONChT5TkcbUoiVUaZM\nJTlPx2hllAiidgF84McqoUKJ4SvsY57Df8dC9VTJ10ZY0tIGlIdtUMfCL6znM55E\ndFs6iMYgA3Cp+KAuGKO5lzzamHO7qjwlBdv0y3l6SbcbCQv/eTOhUzalAoGBAIny\nASxdAd63tpwwNMlJ8NrDlqt/YrZ2L2KGCS5ZZ0GwqH932pJ/W/0s/TAUy3TScWR4\nlR0L285oasIjKPnAZFkjB6YB0roSodLP/38x6r5G0cwLugGM+vuVgn/2VFDOZTk7\n+K9um9pWXY0j9OOy+YKWcAcUsWawNFMkZBy6o8/pAoGAKTKyn46kbV4SVxvQ7bdZ\n6fh+KIzm07dkMB4RkVFtlgGo3B/QIod5xLaiae4k08RbMoP1JfWyEJW7gatIyzbl\noi520z3fsTbiZyQe7VQzPHya3cITVNP54jlLNdstxtPCbVlhGOlJdNtFCserucgY\n0Hawpjt8mceQAfVmivLOsJo=\n-----END PRIVATE KEY-----\n'
   })
 });
 
@@ -26,7 +26,7 @@ app.post('/send-reset', async (req, res) => {
     const r = await fetch('https://api.resend.com/emails', {
       method: 'POST',
       headers: {
-        'Authorization': 're_ighSupGH_AD7nnC83TDS4XnSaDFHfhtz3',
+        'Authorization': 'Bearer re_2MmZ1oTH_9RXu5D9wN6Xxdgc3N8LmVBm4',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
